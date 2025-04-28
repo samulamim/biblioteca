f819df3fa5aec1c6ad5f8828661c7f568b1946dc
samulamim <samuellamim2018@gmail.com>
2025-04-28 10:02:31 -0300

    Add files via upload

---
+++ biblioteca/config/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('livros/', LivrosView.as_view(), name='livros'),
+    path('reserva/', EmprestimoView.as_view(), name='reserva'),
+    path('cidade/', CidadesView.as_view(), name='cidade'),
+    path('autor/', AutoresView.as_view(), name='autor'),
+    path('editor/', EditorasView.as_view(), name='editora'),
+    path('leitor/', LeitoresView.as_view(), name='leitor'),
+    path('genero/', GenerosView.as_view(), name='genero'),
+    # REMOVEI a linha duplicada do ConsultaView
+
+    path('livros/delete/<int:id>/', DeleteLivroView.as_view(), name='delete_livro'),
+]
